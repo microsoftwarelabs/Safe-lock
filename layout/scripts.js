d1b3aa840f61051d3ca7eb5a7701d0d23b54e9bc
Stive Hobbys <149022052+start-hobby@users.noreply.github.com>
2024-02-02 14:41:23 -0300

    Create scripts.js

---
+++ layout/scripts.js
@@ -0,0 +1,19 @@
+function button1Function() {
+	console.log('Button 1 clicked');
+}
+
+function button2Function() {
+	console.log('Button 2 clicked');
+}
+
+function button3Function() {
+	console.log('Button 3 clicked');
+}
+
+function button4Function() {
+	console.log('Button 4 clicked');
+}
+
+function button5Function() {
+	console.log('Button 5 clicked');
+}
